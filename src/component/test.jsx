2dce692bdb53e442f9d400036454ceabc7d1f9cf
vannguyenv12 <npnv.vn1@gmail.com>
2024-05-13 14:59:14 +0700

    feat: Setup base project

---
+++ src/component/test.jsx
@@ -0,0 +1,9 @@
+function Test() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default Test;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { Suspense, lazy } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+
+const App = lazy(() => import("./App"));
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Suspense>
+      <App />
+    </Suspense>
+  </BrowserRouter>
+);
